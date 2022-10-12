0df8d5c63bc681e27f47feb39a5bdcaf1e62b1ff
mehedi556 <mehe556di@gmail.com>
2022-10-12 17:34:09 +0600

    making navbar responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
 
-  <div className="navbar bg-slate-900 px-10">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">QUIZ Time</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal p-0">
-    <Link className='pr-5' to='/'>Home</Link>
-    <Link className='pr-5' to='/statistics'>Statistics</Link>
-    <Link to='/blog'>Blog</Link>
-    </ul>
+  <div className="navbar bg-slate-900">
+  <div className=' w-10/12 mx-auto'>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">QUIZ Time</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal p-0">
+        <Link className='pr-5' to='/'>Home</Link>
+        <Link className='pr-5' to='/statistics'>Statistics</Link>
+        <Link to='/blog'>Blog</Link>
+        </ul>
+      </div>
   </div>
 </div>
     );
